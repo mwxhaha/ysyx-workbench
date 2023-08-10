cd71abab7feb6f143fd4f83722117b0d9f1ba4c1
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-08-10 23:01:18 +0800

    complete exercise 1

--- review-c/exercise1/ex1.c
+++ review-c/exercise1/ex1.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
-    puts("Hello world.");
+    int a = 1;
+    int b[10] = {1, 2, 3, 4, 5};
+    printf("Hello world%d\n",b[a]);
 
     return 0;
 }
