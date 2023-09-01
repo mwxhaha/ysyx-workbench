29f92340b3641c4dd57556e37bcf2fa11d33118c
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-09-01 17:18:22 +0800

    connect to nvboard

--- create_verilator_environment/switch/sim_main.cpp
+++ create_verilator_environment/switch/sim_main.cpp
@@ -1,43 +1,47 @@
 #include "Vtop.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include "nvboard.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
+// #define V_TOP_NAME Vtop
+
+void nvboard_bind_all_pins(V_TOP_NAME* top);
 
 int main(int argc, char **argv)
 {
     VerilatedContext *contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    Vtop *top = new Vtop{contextp};
+    V_TOP_NAME *top = new V_TOP_NAME{contextp};
 
     Verilated::traceEverOn(true); 
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace(tfp, 99); 
     tfp->open("simx.vcd");
 
-    int sim_time = 100;
-    while (contextp->time() < sim_time && !contextp->gotFinish())
-    {
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
+    nvboard_bind_all_pins(top);
+    nvboard_init();
 
+    while (!contextp->gotFinish())
+    {
         contextp->timeInc(1);
         top->eval();
+        assert(top->f == (top->a ^ top->b));
         tfp->dump(contextp->time());
-
-        printf("time = %ld, a = %d, b = %d, f = %d\n", contextp->time(), a, b, top->f);
-        assert(top->f == (a ^ b));
+        nvboard_update();
+        printf("time = %ld, a = %d, b = %d, f = %d\n", contextp->time(), top->a, top->b, top->f);
     }
-
-    tfp->close();
-    delete tfp;
 
     top->final();
     delete top;
     delete contextp;
+
+    tfp->close();
+    delete tfp;
+
+    nvboard_quit();
 
     return 0;
 }
