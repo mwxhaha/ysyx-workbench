36b56d22ad9f5bb40d824cb38dff943a2067aafe
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-09-17 20:14:38 +0800

    expression builder

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,9 +19,12 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
 // this should be enough
 static char buf[65536] = {};
+static int buf_i = 0;
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -31,8 +34,78 @@ static char *code_format =
 "  return 0; "
 "}";
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+static int choose(int n) { return rand() % n; }
+
+static bool gen_num() {
+  int number = rand() % 100;
+  sprintf(buf + buf_i, "%d", number);
+  if (number != 0)
+    buf_i += (int)log10(number) + 1;
+  else
+    buf_i++;
+  if (buf_i >= 65530) return false;
+  return true;
+}
+
+static bool gen(char c) {
+  sprintf(buf + buf_i, "%c", c);
+  buf_i++;
+  if (buf_i >= 65530) return false;
+  return true;
+}
+
+static bool gen_rand_op() {
+  switch (choose(4)) {
+    case 0:
+      sprintf(buf + buf_i, "%c", '+');
+      break;
+    case 1:
+      sprintf(buf + buf_i, "%c", '-');
+      break;
+    case 2:
+      sprintf(buf + buf_i, "%c", '*');
+      break;
+    default:
+      sprintf(buf + buf_i, "%c", '/');
+      break;
+  }
+  buf_i++;
+  if (buf_i >= 65530) return false;
+  return true;
+}
+
+static bool gen_empty() {
+  int n = rand() % 3;
+  for (int i = 1; i <= n; i++) {
+    if (!gen(' ')) return false;
+  }
+  return true;
+}
+
+static bool gen_rand_expr() {
+  switch (choose(3)) {
+    case 0:
+      if (!gen_empty()) return false;
+      if (!gen_num()) return false;
+      if (!gen('u')) return false;
+      if (!gen_empty()) return false;
+      break;
+    case 1:
+      if (!gen('(')) return false;
+      if (!gen_empty()) return false;
+      if (!gen_rand_expr()) return false;
+      if (!gen_empty()) return false;
+      if (!gen(')')) return false;
+      break;
+    default:
+      if (!gen_rand_expr()) return false;
+      if (!gen_empty()) return false;
+      if (!gen_rand_op()) return false;
+      if (!gen_empty()) return false;
+      if (!gen_rand_expr()) return false;
+      break;
+  }
+  return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -44,7 +117,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+    buf_i = 0;
+    if (!gen_rand_expr()) continue;
+    buf[buf_i] = '\0';
 
     sprintf(code_buf, code_format, buf);
 
@@ -53,7 +128,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc /tmp/.code.c -Werror -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
@@ -62,6 +137,7 @@ int main(int argc, char *argv[]) {
     int result;
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
+    if (ret != 1) continue;
 
     printf("%u %s\n", result, buf);
   }
