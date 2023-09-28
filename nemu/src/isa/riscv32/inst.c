de6c46cef057b50963d6d686c89549408c49f759
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-09-28 15:53:09 +0800

    format the code

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -1,55 +1,97 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
+
+#include <common.h>
+#include <cpu/cpu.h>
+#include <cpu/decode.h>
+#include <cpu/ifetch.h>
+#include <memory/vaddr.h>
 
 #include "local-include/reg.h"
-#include <cpu/cpu.h>
-#include <cpu/ifetch.h>
-#include <cpu/decode.h>
 
 #define R(i) gpr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_J, TYPE_S,
-  TYPE_N, // none
+  TYPE_I,
+  TYPE_S,
+  TYPE_U,
+  TYPE_J,
+  TYPE_N,  // none
 };
 
-#define src1R() do { *src1 = R(rs1); } while (0)
-#define src2R() do { *src2 = R(rs2); } while (0)
-#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
-#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 | BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12 ; } while(0)
-#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define src1R()     \
+  do {              \
+    *src1 = R(rs1); \
+  } while (0)
+#define src2R()     \
+  do {              \
+    *src2 = R(rs2); \
+  } while (0)
+#define immI()                        \
+  do {                                \
+    *imm = SEXT(BITS(i, 31, 20), 12); \
+  } while (0)
+#define immU()                              \
+  do {                                      \
+    *imm = SEXT(BITS(i, 31, 12), 20) << 12; \
+  } while (0)
+#define immJ()                                                       \
+  do {                                                               \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 | \
+           BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12;            \
+  } while (0)
+#define immS()                                               \
+  do {                                                       \
+    *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); \
+  } while (0)
 
-static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
+static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
+                           word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
-  *rd     = BITS(i, 11, 7);
+  *rd = BITS(i, 11, 7);
   switch (type) {
-    case TYPE_I: src1R();          immI(); break;
-    case TYPE_U:                   immU(); break;
-    case TYPE_J:                   immJ(); break;
-    case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_I:
+      src1R();
+      immI();
+      break;
+    case TYPE_S:
+      src1R();
+      src2R();
+      immS();
+      break;
+    case TYPE_U:
+      immU();
+      break;
+    case TYPE_J:
+      immJ();
+      break;
+    case TYPE_N:
+      break;
+    default:
+      panic("the type of instruct is no support");
+      break;
   }
 #ifdef CONFIG_DECODE_RESULT
-  printf("i: " FMT_WORD ", rd: " FMT_WORD ", x[rd]: " FMT_WORD
+  printf("pc: " FMT_WORD ", i: " FMT_WORD ", rd: " FMT_WORD ", x[rd]: " FMT_WORD
          ", rs1: %d, x[rs1]: " FMT_WORD ", rs2: %d, x[rs2]: " FMT_WORD
          ", imm: " FMT_WORD ", type: %d \n",
-         i, *rd, R(*rd), rs1, *src1, rs2, *src2, *imm, type);
+         s->pc, i, *rd, R(*rd), rs1, *src1, rs2, *src2, *imm, type);
 #endif
 }
 
@@ -59,25 +101,35 @@ static int decode_exec(Decode *s) {
   s->dnpc = s->snpc;
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
-#define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
-  __VA_ARGS__ ; \
-}
+#define INSTPAT_MATCH(s, name, type, ... /* execute body */)         \
+  {                                                                  \
+    decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+    __VA_ARGS__;                                                     \
+  }
 
   INSTPAT_START();
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
-  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
-  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = imm + src1);
-  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
-  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->pc + 4;s->dnpc=s->pc+imm);
-  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, word_t t = s->pc + 4; s->dnpc = (src1 + imm) & 0xfffffffe;R(rd) = t);
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc, U,
+          R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(rd) = s->pc + 4;
+          s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr, I,
+          word_t t = s->pc + 4;
+          s->dnpc = (src1 + imm) & 0xfffffffe; R(rd) = t);
+  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu, I,
+          R(rd) = Mr(src1 + imm, 1));
+  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb, S,
+          Mw(src1 + imm, 1, src2));
+  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw, S,
+          Mw(src1 + imm, 4, src2));
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I,
+          R(rd) = imm + src1);
 
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N,
+          NEMUTRAP(s->pc, R(10)));  // R(10) is $a0
+  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
   INSTPAT_END();
 
-  R(0) = 0; // reset $zero to 0
+  R(0) = 0;  // reset $zero to 0
 
   return 0;
 }
