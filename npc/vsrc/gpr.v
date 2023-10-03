ea1a25fe81084d1c0cad9d0dc29f6a5505faf75b
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-03 18:06:18 +0800

    refactor module gpr

--- npc/vsrc/gpr.v
+++ npc/vsrc/gpr.v
@@ -1,23 +1,20 @@
+`include "vsrc/config.v"
 module gpr
-    #(
-         parameter ISA_WIDTH = 32,
-         parameter REGISTER_NUM_WIDTH = 5
-     )
      (
          input wire clk,rst,
-         input wire [ISA_WIDTH-1:0] wdata,
-         input wire [REGISTER_NUM_WIDTH-1:0] waddr,
-         output wire [ISA_WIDTH-1:0] rdata_1,
-         input wire [REGISTER_NUM_WIDTH-1:0] raddr_1,
-         output wire [ISA_WIDTH-1:0] rdata_2,
-         input wire [REGISTER_NUM_WIDTH-1:0] raddr_2,
+         input wire [`ISA_WIDTH-1:0] wdata,
+         input wire [`REG_ADDR_WIDTH -1:0] waddr,
+         output wire [`ISA_WIDTH-1:0] rdata_1,
+         input wire [`REG_ADDR_WIDTH -1:0] raddr_1,
+         output wire [`ISA_WIDTH-1:0] rdata_2,
+         input wire [`REG_ADDR_WIDTH -1:0] raddr_2,
          input wire wen
      );
 
     RegisterFile
         #(
-            .ADDR_WIDTH(REGISTER_NUM_WIDTH),
-            .DATA_WIDTH(ISA_WIDTH)
+            .ADDR_WIDTH(`REG_ADDR_WIDTH ),
+            .DATA_WIDTH(`ISA_WIDTH)
         )
         registerfile_gpr
         (
