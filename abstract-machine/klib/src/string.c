6beec6c39087b7fa06972d9e7389ae554d332d89
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-07 15:32:28 +0800

    format code

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,17 +1,14 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  panic("Not implemented");
-}
+size_t strlen(const char *s) { panic("Not implemented"); }
 
 char *strcpy(char *dst, const char *src) {
   int i = 0;
-  while (src[i] != '\0') 
-  {
+  while (src[i] != '\0') {
     dst[i] = src[i];
     i++;
   }
@@ -27,19 +24,17 @@ char *strcat(char *dst, const char *src) {
   int i = 0;
   while (dst[i] != '\0') i++;
   int j = 0;
-  while (src[j] != '\0') 
-  {
-    dst[i+j] = src[j];
+  while (src[j] != '\0') {
+    dst[i + j] = src[j];
     j++;
   }
-  dst[i+j] = src[j];
+  dst[i + j] = src[j];
   return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
   int i = 0;
-  while (s1[i]!='\0' && s2[i]!='\0')
-  {
+  while (s1[i] != '\0' && s2[i] != '\0') {
     if (s1[i] < s2[i]) return -1;
     if (s1[i] > s2[i]) return 1;
     i++;
@@ -64,9 +59,7 @@ void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
+void *memcpy(void *out, const void *in, size_t n) { panic("Not implemented"); }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
   for (int i = 0; i < n; i++) {
