19cfaa239a6b9af05c9c0ea9c102367c39606265
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-07 15:31:07 +0800

    pass string

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,14 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  int i = 0;
+  while (src[i] != '\0') 
+  {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = src[i];
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +24,29 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int i = 0;
+  while (dst[i] != '\0') i++;
+  int j = 0;
+  while (src[j] != '\0') 
+  {
+    dst[i+j] = src[j];
+    j++;
+  }
+  dst[i+j] = src[j];
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int i = 0;
+  while (s1[i]!='\0' && s2[i]!='\0')
+  {
+    if (s1[i] < s2[i]) return -1;
+    if (s1[i] > s2[i]) return 1;
+    i++;
+  }
+  if (s1[i] < s2[i]) return -1;
+  if (s1[i] > s2[i]) return 1;
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +54,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    ((char *)s)[i] = (char)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +69,11 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    if (((char *)s1)[i] < ((char *)s2)[i]) return -1;
+    if (((char *)s1)[i] > ((char *)s2)[i]) return 1;
+  }
+  return 0;
 }
 
 #endif
