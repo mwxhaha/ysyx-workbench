cdfa8942545116080cf444b6f41b36d45ae193c1
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-09 10:55:36 +0800

    format code

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -1,21 +1,21 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <elf.h>
 #include <isa.h>
 #include <memory/paddr.h>
-#include <elf.h>
 #include <stdio.h>
 #include <utils.h>
 
@@ -28,15 +28,18 @@ void init_sdb();
 void init_disasm(const char *triple);
 
 static void welcome() {
-  Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
-  IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
-        "to record the trace. This may lead to a large log file. "
-        "If it is not necessary, you can disable it in menuconfig"));
+  Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN),
+                          ANSI_FMT("OFF", ANSI_FG_RED)));
+  IFDEF(CONFIG_TRACE,
+        Log("If trace is enabled, a log file will be generated "
+            "to record the trace. This may lead to a large log file. "
+            "If it is not necessary, you can disable it in menuconfig"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
-  printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
+  printf("Welcome to %s-NEMU!\n",
+         ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
-  // Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  // assert(0);
+  // Log("Exercise: Please remove me in the source code and compile NEMU
+  // again."); assert(0);
 }
 
 #ifndef CONFIG_TARGET_AM
@@ -52,7 +55,7 @@ static int difftest_port = 1234;
 static long load_img() {
   if (img_file == NULL) {
     Log("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
+    return 4096;  // built-in image size
   }
 
   FILE *fp = fopen(img_file, "rb");
@@ -89,61 +92,63 @@ char shstr[SYM_NAME_MAX];
 #define FUNC_NAME_MAX 100
 #define FUNC_INFOS_MAX 100
 typedef struct {
-Elf32_Addr start_addr;
-Elf32_Addr end_addr;
-char func_name[FUNC_NAME_MAX];
+  Elf32_Addr start_addr;
+  Elf32_Addr end_addr;
+  char func_name[FUNC_NAME_MAX];
 } func_info_t;
 func_info_t func_infos[FUNC_INFOS_MAX];
-int func_infos_max=0;
+int func_infos_max = 0;
 
 static void load_elf() {
   strcpy(elf_file, img_file);
-  elf_file[strlen(elf_file)-1] = 'f';
-  elf_file[strlen(elf_file)-2] = 'l';
-  elf_file[strlen(elf_file)-3] = 'e';
+  elf_file[strlen(elf_file) - 1] = 'f';
+  elf_file[strlen(elf_file) - 2] = 'l';
+  elf_file[strlen(elf_file) - 3] = 'e';
   FILE *fp = fopen(elf_file, "rb");
   Assert(fp, "Can not open '%s'", elf_file);
 
   Elf_Ehdr elf_header;
   int ret = fread(&elf_header, sizeof(Elf_Ehdr), 1, fp);
-  Assert(ret == 1,"elf_header read failed");
-  
+  Assert(ret == 1, "elf_header read failed");
+
   fseek(fp, elf_header.e_shoff, SEEK_SET);
   Assert(elf_header.e_shnum <= SECTION_HEADERS_MAX, "too many sections");
   ret = fread(&section_headers, sizeof(Elf_Shdr), elf_header.e_shnum, fp);
-  Assert(ret == elf_header.e_shnum,"section_headers read failed");
+  Assert(ret == elf_header.e_shnum, "section_headers read failed");
 
-  int sym_tab_max=0;
-  int e_strndx=0;
+  int sym_tab_max = 0;
+  int e_strndx = 0;
   for (int i = 0; i < elf_header.e_shnum; i++) {
     fseek(fp,
           section_headers[elf_header.e_shstrndx].sh_offset +
               section_headers[i].sh_name,
           SEEK_SET);
-    char *ret1 = fgetstr(shstr,SYM_NAME_MAX,fp);
-    Assert(ret1,"shstr read failed");
+    char *ret1 = fgetstr(shstr, SYM_NAME_MAX, fp);
+    Assert(ret1, "shstr read failed");
 
-    if (strcmp(shstr,".symtab")==0) {
+    if (strcmp(shstr, ".symtab") == 0) {
       sym_tab_max = section_headers[i].sh_size / sizeof(Elf_Sym);
-      Assert(sym_tab_max < SYM_TAB_MAX,"too many symbols");
+      Assert(sym_tab_max < SYM_TAB_MAX, "too many symbols");
       fseek(fp, section_headers[i].sh_offset, SEEK_SET);
       ret = fread(sym_tab, sizeof(Elf_Sym), sym_tab_max, fp);
-      Assert(ret == sym_tab_max,"sym_tab read failed");
-   
-    } else if (strcmp(shstr, ".strtab")==0)
+      Assert(ret == sym_tab_max, "sym_tab read failed");
+
+    } else if (strcmp(shstr, ".strtab") == 0)
       e_strndx = i;
   }
 
-  for (int i = 0; i < sym_tab_max;i++) {
-    if ((sym_tab[i].st_info & 0xf)==STT_FUNC) {
+  for (int i = 0; i < sym_tab_max; i++) {
+    if ((sym_tab[i].st_info & 0xf) == STT_FUNC) {
       func_infos[func_infos_max].start_addr = sym_tab[i].st_value;
-      func_infos[func_infos_max].end_addr = sym_tab[i].st_value+sym_tab[i].st_size;
+      func_infos[func_infos_max].end_addr =
+          sym_tab[i].st_value + sym_tab[i].st_size;
       printf("%x %x\n", sym_tab[i].st_name,
              section_headers[e_strndx].sh_offset + sym_tab[i].st_name);
       fseek(fp, section_headers[e_strndx].sh_offset + sym_tab[i].st_name,
             SEEK_SET);
-      char *ret1 = fgetstr(func_infos[func_infos_max].func_name,SYM_NAME_MAX,fp);
-      Assert(ret1,"func_name read failed");
+      char *ret1 =
+          fgetstr(func_infos[func_infos_max].func_name, SYM_NAME_MAX, fp);
+      Assert(ret1, "func_name read failed");
       func_infos_max++;
     }
   }
@@ -153,26 +158,37 @@ static void load_elf() {
 
 static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
-    {"batch"    , no_argument      , NULL, 'b'},
-    {"log"      , required_argument, NULL, 'l'},
-    {"diff"     , required_argument, NULL, 'd'},
-    {"port"     , required_argument, NULL, 'p'},
-    {"help"     , no_argument      , NULL, 'h'},
-    {0          , 0                , NULL,  0 },
+      {"batch", no_argument, NULL, 'b'},
+      {"log", required_argument, NULL, 'l'},
+      {"diff", required_argument, NULL, 'd'},
+      {"port", required_argument, NULL, 'p'},
+      {"help", no_argument, NULL, 'h'},
+      {0, 0, NULL, 0},
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ((o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
-      case 'b': sdb_set_batch_mode(); break;
-      case 'p': sscanf(optarg, "%d", &difftest_port); break;
-      case 'l': log_file = optarg; break;
-      case 'd': diff_so_file = optarg; break;
-      case 1: img_file = optarg; return 0;
+      case 'b':
+        sdb_set_batch_mode();
+        break;
+      case 'p':
+        sscanf(optarg, "%d", &difftest_port);
+        break;
+      case 'l':
+        log_file = optarg;
+        break;
+      case 'd':
+        diff_so_file = optarg;
+        break;
+      case 1:
+        img_file = optarg;
+        return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
         printf("\t-l,--log=FILE           output log to FILE\n");
-        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf(
+            "\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
         printf("\n");
         exit(0);
@@ -214,20 +230,19 @@ void init_monitor(int argc, char *argv[]) {
   init_sdb();
 
 #ifndef CONFIG_ISA_loongarch32r
-  IFDEF(CONFIG_ITRACE, init_disasm(
-    MUXDEF(CONFIG_ISA_x86,     "i686",
-    MUXDEF(CONFIG_ISA_mips32,  "mipsel",
-    MUXDEF(CONFIG_ISA_riscv,
-      MUXDEF(CONFIG_RV64,      "riscv64",
-                               "riscv32"),
-                               "bad"))) "-pc-linux-gnu"
-  ));
+  IFDEF(CONFIG_ITRACE,
+        init_disasm(
+            MUXDEF(CONFIG_ISA_x86, "i686",
+                   MUXDEF(CONFIG_ISA_mips32, "mipsel",
+                          MUXDEF(CONFIG_ISA_riscv,
+                                 MUXDEF(CONFIG_RV64, "riscv64", "riscv32"),
+                                 "bad"))) "-pc-linux-gnu"));
 #endif
 
   /* Display welcome message. */
   welcome();
 }
-#else // CONFIG_TARGET_AM
+#else  // CONFIG_TARGET_AM
 static long load_img() {
   extern char bin_start, bin_end;
   size_t size = &bin_end - &bin_start;
