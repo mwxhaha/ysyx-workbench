14a979ffeddc1a8dc2e460ccf8962f350c7c68a8
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-10 13:49:00 +0800

    refactor printf and sprintf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,71 +8,14 @@
 int printf(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+  char out[100];
+  int ret = vsprintf(out, fmt, ap);
   int i = 0;
-  int j = 0;
-  while (fmt[i] != '\0') {
-    if (fmt[i] == '%') {
-      switch (fmt[i + 1]) {
-        case 'd':
-          int d = va_arg(ap, int);
-          char d_str[20];
-          int k = 0;
-          if (d == 0) {
-            putch('0');
-            j++;
-            break;
-          }
-          if (d < 0) {
-            putch('-');
-            j++;
-            d = -d;
-          }
-          while (d != 0) {
-            d_str[k] = d % 10;
-            k++;
-            d = d / 10;
-          }
-          k--;
-          while (k >= 0) {
-            putch('0' + d_str[k]);
-            k--;
-            j++;
-          }
-          break;
-        case 's':
-          char *s = va_arg(ap, char *);
-          int k1 = 0;
-          while (s[k1] != '\0') {
-            putch(s[k1]);
-            j++;
-            k1++;
-          }
-          putch(s[k1]);
-          j++;
-          break;
-        default:
-          panic("Not implemented");
-          break;
-      }
-      i += 2;
-    } else {
-      putch(fmt[i]);
-      i++;
-      j++;
-    }
-  }
-  putch(fmt[i]);
-  va_end(ap);
-  return j;
+  while (out[i] != '\0') putch(out[i]);
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
   int i = 0;
   int j = 0;
   while (fmt[i] != '\0') {
@@ -126,9 +69,15 @@ int sprintf(char *out, const char *fmt, ...) {
       j++;
     }
   }
-  out[j] = fmt[i];
+  out[j] = '\0';
   va_end(ap);
   return j;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  return vsprintf(out, fmt, ap);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
