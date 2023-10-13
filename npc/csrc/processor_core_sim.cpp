13b642355ad67e5a2440c8717e056d32a7fe90ef
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-13 13:55:54 +0800

    refactor sim frame

--- npc/csrc/processor_core_sim.cpp
+++ npc/csrc/processor_core_sim.cpp
@@ -5,37 +5,6 @@
 #include <iostream>
 #include <cstdint>
 #include <cassert>
-#include <Vtop__Dpi.h>
-
-void pmem_read(int raddr, int *rdata)
-{
-    assert((vaddr_t)raddr >= MEM_BASE_ADDR);
-    assert((vaddr_t)raddr <= MEM_BASE_ADDR + MEM_MAX - 1);
-    void *addr_real = (vaddr_t)raddr - MEM_BASE_ADDR + mem;
-    *rdata = *(word_t *)addr_real;
-}
-
-void pmem_write(int waddr, int wdata, char wmask)
-{
-    assert((vaddr_t)waddr >= MEM_BASE_ADDR);
-    assert((vaddr_t)waddr <= MEM_BASE_ADDR + MEM_MAX - 1);
-    void *addr_real = (vaddr_t)waddr - MEM_BASE_ADDR + mem;
-    switch (wmask)
-    {
-    case 1:
-        *(uint8_t *)addr_real = wdata;
-        break;
-    case 3:
-        *(uint16_t *)addr_real = wdata;
-        break;
-    case 7:
-        *(uint32_t *)addr_real = wdata;
-        break;
-    default:
-        assert(0);
-        break;
-    }
-}
 
 void sim()
 {
--- npc/csrc/sim_tool.cpp
+++ npc/csrc/sim_tool.cpp
@@ -122,3 +122,33 @@ void ebreak_dpic(int ret,int pc)
     npc_state.state = end;
     npc_state.pc = pc;
 }
+
+void pmem_read(int raddr, int *rdata)
+{
+    assert((vaddr_t)raddr >= MEM_BASE_ADDR);
+    assert((vaddr_t)raddr <= MEM_BASE_ADDR + MEM_MAX - 1);
+    void *addr_real = (vaddr_t)raddr - MEM_BASE_ADDR + mem;
+    *rdata = *(word_t *)addr_real;
+}
+
+void pmem_write(int waddr, int wdata, char wmask)
+{
+    assert((vaddr_t)waddr >= MEM_BASE_ADDR);
+    assert((vaddr_t)waddr <= MEM_BASE_ADDR + MEM_MAX - 1);
+    void *addr_real = (vaddr_t)waddr - MEM_BASE_ADDR + mem;
+    switch (wmask)
+    {
+    case 1:
+        *(uint8_t *)addr_real = wdata;
+        break;
+    case 3:
+        *(uint16_t *)addr_real = wdata;
+        break;
+    case 7:
+        *(uint32_t *)addr_real = wdata;
+        break;
+    default:
+        assert(0);
+        break;
+    }
+}
