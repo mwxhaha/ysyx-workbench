de992d197854aa6f0fb581a9418d154ca2ad950b
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2023-10-28 14:32:51 +0800

    am support vga, pass am-tests display test

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -7,15 +8,30 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  int i;
+  int w = inw(VGACTL_ADDR + 2); // TODO: get the correct width
+  int h = inw(VGACTL_ADDR);     // TODO: get the correct height
+  for (i = 0; i < w * h; i++)
+  {
+    outl(FB_ADDR + i * 4, 0);
+  }
+  outl(SYNC_ADDR, 1);
+  *cfg = (AM_GPU_CONFIG_T){.present = true, .has_accel = false, .width = w, .height = h, .vmemsz = w * h * 4};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+  if (ctl->pixels)
+  {
+    int width = inw(VGACTL_ADDR + 2);
+    outl(SYNC_ADDR, 1);
+    for (int i = 0; i < ctl->w; i++)
+      for (int j = 0; j < ctl->h; j++)
+      {
+        outl(FB_ADDR + ((ctl->y + j) * width + ctl->x + i) * 4, ((uint32_t *)(ctl->pixels))[j * ctl->w + i]);
+      }
+  }
+  if (ctl->sync)
+  {
     outl(SYNC_ADDR, 1);
   }
 }
