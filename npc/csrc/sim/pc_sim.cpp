74e79b115d69ef54c9e56ec5125667d0697f83ab
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2024-02-11 15:48:41 +0800

    pc_sim

--- npc/csrc/sim/pc_sim.cpp
+++ npc/csrc/sim/pc_sim.cpp
@@ -1,32 +1,44 @@
-#include <verilated.h>
-#include <Vtop.h>
-#include <verilated_vcd_c.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <util/debug.hpp>
+#include <util/macro.hpp>
 #include <util/sim_tool.hpp>
-#include <iostream>
-#include <cassert>
 
 void sim()
 {
 #ifdef NV_SIM
-        while (!contextp->gotFinish())
-        {
-                update();
-        }
+    while (!contextp->gotFinish())
+    {
+        update();
+    }
 #else
-        int sim_time = 1000;
-        reset();
-        while (contextp->time() < sim_time && !contextp->gotFinish())
-        {
-                set_pin([&]
-                        { top->pc_in = 1; top->pc_w_en = 1; });
-                set_pin([&]
-                        { top->pc_in = 0; top->pc_w_en = 1; });
-                set_pin([&]
-                        { top->pc_in = 1; top->pc_w_en = 0; });
-                set_pin([&]
-                        { top->pc_in = 1; top->pc_w_en = 1; });
-                set_pin([&]
-                        { top->pc_in = 0; top->pc_w_en = 0; });
-        }
+    int sim_time = 1000;
+    reset();
+    while (contextp->time() < sim_time && !contextp->gotFinish())
+    {
+        set_pin([&]
+                { top->pc_in = 0x10000001; top->pc_en = 1; });
+        assert(top->pc_out == 0x10000001);
+        set_pin([&]
+                { top->pc_in = 0x00000000; top->pc_en = 1; });
+        assert(top->pc_out == 0x00000000);
+        set_pin([&]
+                { top->pc_in = 0x10000001; top->pc_en = 0; });
+        assert(top->pc_out == 0x00000000);
+        set_pin([&]
+                { top->pc_in = 0x11111111; top->pc_en = 1; });
+        assert(top->pc_out == 0x11111111);
+        set_pin([&]
+                { top->pc_in = 0x00000000; top->pc_en = 0; });
+        assert(top->pc_out == 0x11111111);
+    }
 #endif
 }
