cd9c56b3575f3f9da2c4a9b37aab65421b9a46f2
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2024-02-11 21:15:19 +0800

    alu_sim

--- npc/csrc/sim/alu_sim.cpp
+++ npc/csrc/sim/alu_sim.cpp
@@ -1,7 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <util/debug.hpp>
+#include <util/macro.hpp>
+#include <util/sim_tool.hpp>
 #include <verilated.h>
 #include <Vtop.h>
-#include <verilated_vcd_c.h>
-#include <util/sim_tool.hpp>
+
+#define ALU_TEST_LOOP(X)                        \
+    do                                          \
+    {                                           \
+        uint32_t i = 0;                         \
+        bool i_last_flag = true;                \
+        while (i_last_flag)                     \
+        {                                       \
+            uint32_t j = 0;                     \
+            bool j_last_flag = true;            \
+            while (j_last_flag)                 \
+            {                                   \
+                top->alu_a = i;                 \
+                top->alu_b = j;                 \
+                update();                       \
+                assert(top->alu_result == (X)); \
+                if (j == 0xffffffff)            \
+                    j_last_flag = false;        \
+                j += 0x11111111;                \
+            }                                   \
+            if (i == 0xffffffff)                \
+                i_last_flag = false;            \
+            i += 0x11111111;                    \
+        }                                       \
+    } while (0)
 
 void sim()
 {
@@ -11,18 +47,37 @@ void sim()
         update();
     }
 #else
-    unsigned int sim_time = 10000;
+    int sim_time = 10000;
     while (contextp->time() < sim_time && !contextp->gotFinish())
     {
-        for (unsigned int i = 0; i <= 15; ++i)
-            for (unsigned int j = 0; j <= 15; ++j)
-                for (unsigned int k = 0; k <= 15; ++k)
-                {
-                    top->alu_a = j;
-                    top->alu_b = k;
-                    top->alu_func = i;
-                    update();
-                }
+        top->alu_funct = 0;
+        ALU_TEST_LOOP(i + j);
+        top->alu_funct = 8;
+        ALU_TEST_LOOP(i - j);
+        top->alu_funct = 10;
+        ALU_TEST_LOOP((int32_t)i < (int32_t)j);
+        top->alu_funct = 11;
+        ALU_TEST_LOOP(i < j);
+        top->alu_funct = 1;
+        ALU_TEST_LOOP(i << (j & 0x1f));
+        top->alu_funct = 5;
+        ALU_TEST_LOOP(i >> (j & 0x1f));
+        top->alu_funct = 13;
+        ALU_TEST_LOOP((int32_t)i >> (j & 0x1f));
+        top->alu_funct = 4;
+        ALU_TEST_LOOP(i ^ j);
+        top->alu_funct = 6;
+        ALU_TEST_LOOP(i | j);
+        top->alu_funct = 7;
+        ALU_TEST_LOOP(i & j);
+        top->alu_funct = 9;
+        ALU_TEST_LOOP(i == j);
+        top->alu_funct = 12;
+        ALU_TEST_LOOP(i != j);
+        top->alu_funct = 14;
+        ALU_TEST_LOOP((int32_t)i >= (int32_t)j);
+        top->alu_funct = 15;
+        ALU_TEST_LOOP(i >= j);
     }
 #endif
 }
