f7d4b720c6f88fd1c754228e394cbe924a7dcea8
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2024-02-12 11:30:37 +0800

    refactor cpu.hpp

--- npc/include/sim/cpu.hpp
+++ npc/include/sim/cpu.hpp
@@ -23,39 +23,41 @@
 
 #if CONFIG_ISA == CONFIG_RV32I
 #define ISA_WIDTH 32
-typedef uint32_t word_t;
-typedef int32_t sword_t;
-#define FMT_WORD "0x%08x"
-#define FMT_WORD_T "%u"
-#define FMT_SWORD_T "%d"
 #define GPR_NUM 32
 #define INST_LEN 32
-typedef uint32_t inst_t;
-#define FMT_INST "0x%08x"
 #elif CONFIG_ISA == CONFIG_RV32E
 #define ISA_WIDTH 32
+#define GPR_NUM 16
+#define INST_LEN 32
+#elif CONFIG_ISA == CONFIG_RV64I
+#define ISA_WIDTH 64
+#define GPR_NUM 32
+#define INST_LEN 32
+#else
+#error "do not support ISA " #CONFIG_ISA
+#endif
+
+#if ISA_WIDTH == 32
 typedef uint32_t word_t;
 typedef int32_t sword_t;
 #define FMT_WORD "0x%08x"
 #define FMT_WORD_T "%u"
 #define FMT_SWORD_T "%d"
-#define GPR_NUM 16
-#define INST_LEN 32
-typedef uint32_t inst_t;
-#define FMT_INST "0x%08x"
-#elif CONFIG_ISA == CONFIG_RV64I
-#define ISA_WIDTH 64
+#elif ISA_WIDTH == 64
 typedef uint64_t word_t;
 typedef int64_t sword_t;
 #define FMT_WORD "0x%016lx"
 #define FMT_WORD_T "%lu"
 #define FMT_SWORD_T "%ld"
-#define GPR_NUM 32
-#define INST_LEN 32
+#else
+#error "do not support ISA_WIDTH " #ISA_WIDTH
+#endif
+
+#if INST_LEN == 32
 typedef uint32_t inst_t;
 #define FMT_INST "0x%08x"
 #else
-#error "do not support ISA " #CONFIG_ISA
+#error "do not support INST_LEN " #INST_LEN
 #endif
 
 typedef word_t vaddr_t;
