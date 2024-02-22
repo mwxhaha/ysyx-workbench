58124fbf5db2be3b14e5f5f52896c95d5c2abd42
Meng Weixuan <mengwx@shanghaitech.edu.cn>
2024-02-22 20:26:49 +0800

    format code

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -20,18 +20,18 @@
 
 typedef struct
 {
-  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
-  vaddr_t pc;
-  word_t csr[4096];
+    word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
+    vaddr_t pc;
+    word_t csr[4096];
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
 typedef struct
 {
-  union
-  {
-    uint32_t val;
-  } inst;
+    union
+    {
+        uint32_t val;
+    } inst;
 } MUXDEF(CONFIG_RV64, riscv64_ISADecodeInfo, riscv32_ISADecodeInfo);
 
 #define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
